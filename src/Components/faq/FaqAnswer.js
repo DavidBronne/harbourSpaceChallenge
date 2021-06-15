a4d97aada1a73c6d4a01e9f64a9f2d91ce7393cc
David Bronne <davidbronne@gmail.com>
2021-06-15 11:57:22 +0200

    handletoggle and isDisplay in context

--- src/Components/faq/FaqAnswer.js
+++ src/Components/faq/FaqAnswer.js
@@ -4,53 +4,38 @@ import {ApprenticeshipContext} from '../../contexts/ApprenticeshipContext'
 const FaqAnswer = ({selection}) => {
     console.log(`selection`, selection)
     const {data, isPending, error} = useContext(ApprenticeshipContext)
-/*     if (data) {
-        const filteredFaqs = data.scholarship.faqs.items.filter( item => {
-            return item.type === selection
-        })
-        console.log(`filteredFaqs`, filteredFaqs)
-        const elemementToDisplay = filteredFaqs.map( el => {
-            console.log(`el.question`, el.question)
-            return <div>{el.question}</div>
-        })
-    } */
-    const {toggleDetailButton} = useContext(ApprenticeshipContext)
-   
 
-    
+    const {toggleDetailButton} = useContext(ApprenticeshipContext)
     const {isDetailed} = useContext(ApprenticeshipContext)
-    console.log('isDetailed :>> ', isDetailed);
+   
     return <div>
         
         { data && data.scholarship.faqs.items
-                    .filter( item => {
-                        return item.type === selection
-                    })
-                    .map( (el, i) => {
-                        return (
-
-                                
-                                <div className="format-row-item border-top" key={i}>
-                                    <h2 className="format-1third-section">{selection}</h2>
-                                    <div className="format-2third-section" >
-                                        <div>
-                                            <div >{el.question}</div>
-                                            <div onClick={toggleDetailButton} >button</div>
-                                        </div>
-                                        
-                                       { isDetailed  ? (
-                                                <div>
-                                                    {el.answer.map( (an,i) => {
-                                                        return <p className="answer text-small" key={i}>{an.data}</p>
-                                                    })}
-                                                </div>
-                                            ) : <div></div>}
-                                        
-                                    </div>
-                                </div>
+            .filter( item => {
+                return item.type === selection
+            })
+            .map( (el, i) => {
+                return ( 
+                    <div className="format-row-item border-top" key={i}>
+                        <h2 className="format-1third-section">{selection}</h2>
+                        <div className="format-2third-section" >
+                            <div>
+                                <div >{el.question}</div>
+                                <div onClick={toggleDetailButton} >button</div>
+                            </div>
                             
-                    )
-                    })
+                            { isDetailed  ? (
+                                    <div>
+                                        {el.answer.map( (an,i) => {
+                                            return <p className="answer text-small" key={i}>{an.data}</p>
+                                        })}
+                                    </div>
+                                ) : <div></div>}
+                            
+                        </div>
+                    </div>        
+                )
+            })
         }
     </div>
 }
